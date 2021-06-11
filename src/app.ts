e464987dc3776e7adbf7e3da4c519039b346e4e8
Giovana Yokota <gnyokota@gmail.com>
2021-06-11 16:47:57 +0200

    new images pushed

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(apiErrorHandler)
 
 //check if the server is running:
 app.get('/', (req, res) => {
-  res.send('Server is running properly 😊')
+  res.send('Server is running properly 😊😉')
 })
 
 export default app
